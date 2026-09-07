611a7c6a0805ab95c04bbd5febfa6ac07d19bddf
Alireza <124519949+aliwb6@users.noreply.github.com>
2026-09-08 02:29:59 +0330

    Mount mobile homepage primary navigation

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
+import MobileHomePrimaryNav from './MobileHomePrimaryNav';
 import Footer from './Footer';
 
 export default function Layout() {
   return (
     <div className="public-site-shell min-h-screen flex flex-col">
       <Navbar />
+      <MobileHomePrimaryNav />
       <main className="flex-1">
         <Outlet />
       </main>
